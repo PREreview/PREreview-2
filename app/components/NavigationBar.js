056636677d8eb36731117bf0d9f51455139f09b8
Yannis Barlas <yannisbarlas@gmail.com>
2018-08-20 18:05:07 +0300

    fix: change app name to micropublications

--- app/components/NavigationBar.js
+++ app/components/NavigationBar.js
@@ -59,7 +59,7 @@ const NavigationBar = ({
 
   return (
     <AppBar
-      brand="WormBase"
+      brand="MicroPublications"
       navLinkComponents={links}
       onLogoutClick={onLogoutClick}
       user={currentUser}
